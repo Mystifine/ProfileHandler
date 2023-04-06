4f17e2391353e3a211af918b939048a0beee3b62
Mystifine <35544772+Mystifine@users.noreply.github.com>
2023-04-06 13:44:58 -0400

    Update and rename PlayerData.lua to constants.lua

---
+++ constants.lua
@@ -0,0 +1,15 @@
+local constants = {}
+
+constants.MASTER_KEY = "DATA";
+
+constants.DEBUG_MODE = true;
+constants.IGNORE_COOLDOWN_MESSAGE = true;
+constants.STUDIO_SAVE = true;
+
+-- ROBLOX COOLDOWNS ARE 6 SECONDS BETWEEN REQUESTS. YOU DON'T NEED SUCH A SHORT COOLDOWN.
+constants.REQUEST_COOLDOWNS = {
+	WRITE = 10, 
+	READ = 10,	
+}
+
+return constants
